886ef5a29c1d73bc0501d2bf4e6db61ac727ddd5
Joao P Selva <jpselva@usp.br>
2023-03-11 22:49:35 -0300

    Modified control unit

--- src/unidade_controle.vhd
+++ src/unidade_controle.vhd
@@ -3,41 +3,44 @@ use ieee.std_logic_1164.all;
 
 entity unidade_controle is 
     port ( 
-        clock               : in  std_logic; 
-        reset               : in  std_logic; 
-        iniciar             : in  std_logic;
+        -- inputs
+        clock               : in std_logic; 
+        reset               : in std_logic; 
+        iniciar             : in std_logic;
         jogada_feita        : in std_logic;
-        jogada_correta      : in  std_logic;
-        fimL                : in  std_logic;
-        fimE                : in  std_logic;
-        fimT                : in  std_logic;
-        fimTI                : in  std_logic;
-        enderecoIgualRodada : in std_logic;
-        escreveM  : out std_logic;
-        zeraE     : out std_logic;
-        contaE    : out std_logic;
-        limpaRC   : out std_logic;
-        registraRC: out std_logic;
-        zeraD     : out std_logic;
-        contaCR   : out std_logic;
-        zeraCR    : out std_logic;
-        leds_src  : out std_logic;
-        contaT    : out std_logic;
-        contaTI   : out std_logic;
-        zeraT     : out std_logic;
-        zeraTI    : out std_logic;
-        acertou   : out std_logic;
-        errou     : out std_logic;
-        pronto    : out std_logic;
-        db_timeout: out std_logic;
-        db_estado : out std_logic_vector(3 downto 0)
+        jogada_correta      : in std_logic;
+        fim_contjog         : in std_logic;
+        timeout_tnota       : in std_logic;
+        timeout_tsil        : in std_logic;
+
+        -- outputs
+        zera_contjog        : out std_logic;
+        conta_contjog       : out std_logic;
+        zera_regnota        : out std_logic;
+        registra_nota       : out std_logic;
+        zera_regmasc        : out std_logic;
+        registra_regmasc    : out std_logic;
+        masc_dado           : out std_logic_vector(11 downto 0);
+        nota_src            : out std_logic;
+        zera_conterros      : out std_logic;
+        conta_conterros     : out std_logic;
+        zera_tnota          : out std_logic;
+        conta_tnota         : out std_logic;
+        zera_tsil           : out std_logic;
+        conta_tsil          : out std_logic;
+        zera_detec          : out std_logic;
+        toca_nota           : out std_logic;
+        pronto              : out std_logic;
+
+        -- debug
+        db_estado           : out std_logic_vector(3 downto 0)
     );
 end entity;
 
 architecture fsm of unidade_controle is
-    type t_estado is (inicial, preparacao, inicio_rodada, espera, registra, 
-                      comparacao, proxima_jogada, terminou_rodada, proxima_rodada, 
-                      fim_acertou, fim_errou, fim_timeout, espera_escrita);
+    type t_estado is (inicial, preparacao, toca, espera, registra, 
+                      comparacao, espera_errou, espera_acertou, errou_jogada, 
+                      acertou_jogada, espera_silencio, fim);
     signal Eatual, Eprox : t_estado;
 begin
 
@@ -52,36 +55,27 @@ begin
     end process;
 
     -- logica de proximo estado
-    control: process(Eatual, iniciar, fimT, fimTI, jogada_feita, 
-                     enderecoIgualRodada, fimL, jogada_correta)
+    control: process(iniciar, jogada_feita, jogada_correta, fim_contjog,
+                     timeout_tnota, timeout_tsil)
     begin
-        acertou    <= '0';
-        errou      <= '0';
-        zeraE      <= '0';
-        contaE     <= '0';
-        limpaRC    <= '0';
-        registraRC <= '0';
-        zeraD      <= '0';
-        contaCR    <= '0';
-        zeraCR     <= '0';
-        leds_src   <= '0';
-        pronto     <= '0';
-        contaT     <= '0';
-        contaTI    <= '0';
-        zeraT      <= '0';
-        zeraTI     <= '0';
-        db_timeout <= '0';
-        escreveM   <= '0';
+        zera_contjog <= '0';
+        conta_contjog <= '0';
+        zera_regnota <= '0';
+        registra_nota <= '0';
+        zera_regmasc <= '0';
+        registra_regmasc <= '0';
+        masc_dado <= "000000000000";
+        nota_src <= '0';
+        zera_conterros <= '0';
+        conta_conterros <= '0';
+        zera_tnota <= '0';
+        conta_tnota <= '0';
+        zera_tsil <= '0';
+        conta_tsil <= '0';
 
         case Eatual is
 
             when inicial => 
-                zeraE   <= '1';
-                zeraT   <= '1';
-                zeraTI  <= '1';
-                limpaRC <= '1';
-                zeraD   <= '1';
-
                 if ( iniciar = '0' ) then
                     Eprox <= inicial;
                 else
@@ -89,111 +83,102 @@ begin
                 end if;
             
             when preparacao =>
-                contaTI <= '1';
-                zeraCR  <= '1';
-                leds_src<= '1';
+                zera_contjog <= '1';
+                zera_regnota <= '1';
+                zera_conterros <= '1';
+                zera_tnota <= '1';
+                zera_tsil <= '1';
+                zera_detec <= '1';
+                registra_regmasc <= '1';
+                masc_dado <= "000000000111";
                 
-                if ( fimTI = '0' ) then
-                    Eprox <= preparacao;
-                else 
-                    Eprox <= inicio_rodada;
-                end if;
-				
-            when inicio_rodada =>
-				zeraE <= '1';
-				zeraT <= '1';
-				zeraTI<= '1';
+                Eprox <= toca;
 
-				Eprox <= espera;
+            when toca =>
+                toca_nota <= '1';
+                nota_src <= '1';
+                zera_tsil <= '1';
+                conta_tnota <= '1';
+
+                if ( timeout_tnota = '0' ) then
+                    Eprox <= toca;
+                else
+                    Eprox <= espera;
+                end if;
 
             when espera =>
-				contaT <= '1';
-
-				if ( fimT = '0' and jogada_feita = '0' ) then
-					Eprox <= espera;
-				elsif ( fimT = '0' and jogada_feita = '1' ) then
-					Eprox <= registra;
-				else 
-					Eprox <= fim_timeout;
-				end if;
+                if ( jogada_feita = '0' ) then
+                    Eprox <= espera;
+                else
+                    Eprox <= registra;
+                end if;
 			
 			when registra =>
-				registraRC <= '1';
-				
+				registra_nota <= '1';
 				Eprox <= comparacao;
 			
 			when comparacao => 
-				zeraT <= '1';
+				zera_tnota <= '1';
 				
-				if ( enderecoIgualRodada = '0' and jogada_correta = '1' ) then
-					Eprox <= proxima_jogada;
-				elsif ( enderecoIgualRodada = '1' and jogada_correta = '1' ) then
-					Eprox <= terminou_rodada;
-				else 
-					Eprox <= fim_errou;
-				end if;
+                if ( jogada_correta = '0' ) then
+                    Eprox <= espera_errou;
+                elsif ( fim_contjog = '1' ) then
+                    Eprox <= fim;
+                else
+                    Eprox <= espera_acertou;
+                end if;
+
+            when espera_acertou =>
+                toca_nota <= '1';
+                conta_tnota <= '1';
+
+                if ( timeout_tnota = '0' ) then
+                    Eprox <= espera_acertou;
+                else
+                    Eprox <= acertou_jogada;
+                end if;
+
+			when acertou_jogada => 
+                zera_tnota <= '1';
+                zera_tsil <= '1';
+                conta_contjog <= '1';
+
+                Eprox <= espera_silencio;
+
+			when espera_errou =>
+                toca_nota <= '1';
+                conta_tnota <= '1';
+
+                if ( timeout_tnota = '0' ) then
+                    Eprox <= espera_errou;
+                else
+                    Eprox <= errou_jogada;
+                end if;
+
+            when errou_jogada =>
+                zera_tnota <= '1';
+                zera_tsil <= '1';
+                conta_conterros <= '1';
+
+                Eprox <= espera_silencio;
             
-			when proxima_jogada =>
-				contaE <= '1';
+            when espera_silencio =>
+                conta_tsil <= '1';
 
-				Eprox <= espera;
+                if ( timeout_tsil = '0' ) then
+                    Eprox <= espera_silencio;
+                else
+                    Eprox <= toca;
+                end if;
 
-			when terminou_rodada => 
-				contaE <= '1';
-				if ( fimL = '0' ) then
-					Eprox <= espera_escrita;
-				else 
-					Eprox <= fim_acertou;
-				end if;
-            
-            when espera_escrita =>
-				contaT <= '1';
-                registraRC <= '1';
-				if ( fimT = '0' and jogada_feita = '0' ) then
-					Eprox <= espera_escrita;
-				elsif ( fimT = '0' and jogada_feita = '1' ) then
-					Eprox <= proxima_rodada;
-				else 
-					Eprox <= fim_timeout;
-				end if;
-			
+            when fim =>
+                pronto <= '1';
 
-			when proxima_rodada =>
-				contaCR <= '1';
-                escreveM <= '1';
-				Eprox <= inicio_rodada;
-				
-			when fim_timeout =>
-				errou 	   <= '1';
-				pronto 	   <= '1';
-				db_timeout <= '1';
-				zeraT 	   <= '1';
-			
-				if ( iniciar = '1' ) then
-					Eprox <= preparacao;
-				else 
-					Eprox <= fim_timeout;
-				end if;
-
-			when fim_errou =>
-				errou <= '1';
-				pronto <= '1';
-
-				if ( iniciar = '1' ) then
-					Eprox <= preparacao;
-				else 
-					Eprox <= fim_errou;
-				end if;
-
-			when fim_acertou =>
-				acertou <= '1';
-				pronto <= '1';
-
-				if ( iniciar = '1' ) then
-					Eprox <= preparacao;
-				else 
-					Eprox <= fim_acertou;
-				end if;
+                if ( iniciar = '1' ) then
+                    Eprox <= preparacao;
+                else
+                    Eprox <= fim;
+                end if;
 			
         end case;
     end process;
@@ -202,17 +187,16 @@ begin
     with Eatual select
         db_estado <= "0000" when inicial,         -- 0
                      "0001" when preparacao,      -- 1
-                     "0010" when inicio_rodada,   -- 2
                      "0011" when registra,        -- 3
                      "0100" when comparacao,      -- 4
-                     "0101" when proxima_jogada,  -- 5
-                     "0110" when terminou_rodada, -- 6
-                     "0111" when proxima_rodada,  -- 7
-                     "1000" when fim_timeout,     -- 8
-                     "1010" when fim_acertou,     -- A
-                     "1110" when fim_errou,       -- E
+                     "0101" when errou_jogada,    -- 5
+                     "0110" when acertou_jogada,  -- 6
+                     "0111" when espera_silencio, -- 7
+                     "1000" when fim,             -- 8
+                     "1010" when espera_acertou,  -- A
+                     "1100" when toca,            -- C
                      "1101" when espera,          -- D
-                     "1011" when espera_escrita,  -- B
+                     "1110" when espera_errou,    -- E
                      "1111" when others;          -- F
 
 end architecture fsm;
