6653e1f30837cac66907591b170006afe574d878
Joao P Selva <jpselva@usp.br>
2023-03-22 21:54:13 -0300

    Refactor gerador_freq

--- src/gerador_freq.vhd
+++ src/gerador_freq.vhd
@@ -1,4 +1,3 @@
---
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -30,22 +29,18 @@ architecture arch of gerador_freq is
 		 );
 	end component;
 
-
 	signal s_notas, tom : std_logic_vector(11 downto 0);
 	signal s_notas_not : std_logic_vector(11 downto 0);
 
-
 begin
+    s_notas <= nota;
+    s_notas_not <= not s_notas;
 
-   s_notas <= nota;
-	s_notas_not <= not s_notas;
-
-	
 	do4 : contador_m_maior
-		generic map(
+    generic map (
         M => 191110 -- do da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(0),
         zera_s  => '0',
@@ -53,13 +48,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(0)
-		);
-	
+    );
+
 	re4 : contador_m_maior
-		generic map(
+    generic map (
         M => 170265 -- re da quarta oitava
-		)
-		port map(
+    )
+    port map(
         clock   => clock,
         zera_as => s_notas_not(1),
         zera_s  => '0',
@@ -67,13 +62,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(1)
-		);
-		
+    );
+
 	mi4 : contador_m_maior
-		generic map(
+    generic map (
         M => 151685 -- mi da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(2),
         zera_s  => '0',
@@ -81,13 +76,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(2)
-		);
-		
+    );
+
 	fa4 : contador_m_maior
-		generic map(
+    generic map (
         M => 143172 -- fa da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(3),
         zera_s  => '0',
@@ -95,13 +90,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(3)
-		);
-	
+    );
+
 	sol4 : contador_m_maior
-		generic map(
+    generic map (
         M => 127551 -- sol da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(4),
         zera_s  => '0',
@@ -109,13 +104,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(4)
-		);
-		
+    );
+    
 	la4 : contador_m_maior
-		generic map(
+    generic map (
         M => 113636 -- 50 Mhz / 440 Hz la da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(5),
         zera_s  => '0',
@@ -123,13 +118,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(5)
-		);
-	
+    );
+
 	si4 : contador_m_maior
-		generic map(
+    generic map (
         M => 101239 -- si da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(6),
         zera_s  => '0',
@@ -137,13 +132,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(6)
-		);
-	
+    );
+
     do_m4 : contador_m_maior
-		generic map(
+    generic map (
         M => 180388 -- si da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(7),
         zera_s  => '0',
@@ -151,14 +146,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(7)
-		);
-    
-        
+    );
+
     re_m4 : contador_m_maior
-        generic map(
+    generic map (
         M => 160705 -- si da quarta oitava
-        )
-        port map(
+    )
+    port map(
         clock   => clock,
         zera_as => s_notas_not(8),
         zera_s  => '0',
@@ -166,13 +160,13 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(8)
-        );
-    
+    );
+
     fa_m4 : contador_m_maior
-		generic map(
+    generic map(
         M => 135139 -- si da quarta oitava
-		)
-		port map(
+    )
+    port map (
         clock   => clock,
         zera_as => s_notas_not(9),
         zera_s  => '0',
@@ -180,8 +174,8 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(9)
-		);
-        
+    );
+
     sol_m4 : contador_m_maior
     generic map(
 		M => 120395 -- si da quarta oitava
@@ -195,12 +189,12 @@ begin
 		 fim     => open,
 		 meio    => tom(10)
     );
-    
+
     la_m4 : contador_m_maior
-		generic map(
+    generic map(
         M => 107259 -- si da quarta oitava
-		)
-		port map(
+    )
+    port map(
         clock   => clock,
         zera_as => s_notas_not(11),
         zera_s  => '0',
@@ -208,7 +202,7 @@ begin
         Q       => open,
         fim     => open,
         meio    => tom(11)
-		);
-        
+    );
+
 	saida <= (tom(8) or tom(7) or tom(6) or tom(5) or tom(4) or tom(3) or tom(2) or tom(1) or tom(0) or tom(11) or tom(10) or tom(9)) and toca_nota;
-	end architecture;
+end architecture;
